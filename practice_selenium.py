e0df5693f315f6414ca0b534c3b48932f6479f23
Hector Valdes <valdes.h.hector@gmail.com>
2024-11-07 16:32:14 -0600

    Merge remote-tracking branch 'main_login/master'

--- practice_selenium.py
+++ practice_selenium.py
@@ -16,15 +16,31 @@ try:
     # Explicit wait to ensure the element is present before searching
     timeout = 10  # seconds
     try:
-        # Find the button using aria-label
+        # Find the button using aria-label and press it
         button = WebDriverWait(driver, timeout).until(
             EC.presence_of_element_located((By.XPATH, "//div[@role='button' and @aria-label='Active Directory']"))
         )
-
-        # Click the button (optional, if you just want to verify its presence, remove this line)
         button.click()
 
         print("Active Directory button found and clicked!")
+        #Has succesfully logined to healthchart page
+        time.sleep(10)
+        """ECU_field = WebDriverWait(driver, timeout).until(
+            EC.presence_of_element_located((By.XPATH, "//div[@role='button' and @aria-label='Active Directory']"))
+        )
+        ECU_field = driver.find_element(By.ID, "colum-ecu-filer")  # Replace with actual ID
+        ECU_field.send_keys("RGTM")"""
+        ecu_input = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//input[@type='text' and contains(@class, 'inputwidth')]"))
+        )
+
+        # Clear the existing text in the input field
+        ecu_input.clear()
+
+        # Send the text "RGTM" to the input field
+        ecu_input.send_keys("RGTM")
+
+        print("Text 'RGTM' entered successfully!")
 
         time.sleep(10)
     except TimeoutException:
